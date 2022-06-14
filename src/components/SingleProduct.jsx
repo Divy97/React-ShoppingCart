26e6108b5ecbfbdab4f0f93fb8acd6d117b23136
Divy Parekh <divyparekh1810@gmail.com>
2022-06-14 22:56:51 +0530

    SingleProduct UI done

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
-
+import {Card, Button} from 'react-bootstrap'
+import Rating from './Rating'
 const SingleProduct = ({prod}) => {
   return (
-    <div>{prod.name}</div>
+    <div className='products'>
+      <Card>
+        <Card.Img variant='top' src={prod.image} alt={prod.name} />
+        <Card.Body>
+          <Card.Title>{prod.name}</Card.Title>
+          <Card.Subtitle style={{ paddingBottom: 10}}>
+              <span>₹ {prod.price.split(".")[0]}</span>
+              {prod.fastDelivery ? (
+                <div> Fast Delivery </div>
+              ) : (
+                <div>4 Days Delivery</div>
+              )}
+              <Rating rating={prod.ratings} />
+          </Card.Subtitle>
+          <Button variant='danger'>
+            Remove From Cart
+          </Button>
+          <Button disabled={!prod.inStock}>
+            {!prod.inStock ? "Out of Stock" : "Add to Cart" }
+          </Button>
+        </Card.Body>
+      </Card>
+    </div>
   )
 }
 
